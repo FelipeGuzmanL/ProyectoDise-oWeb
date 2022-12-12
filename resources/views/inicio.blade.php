ab46250c3807b4c911b01e3a065cd196dd4bfd54
FelipeGuzmanL <felipeeduardo.guzmanlobos@gmail.com>
2022-12-12 13:32:49 -0300

    Imagen logo del jona

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,13 +3,13 @@
 
     <div class="container">
         <div class="card" style="width: 18rem;">
-            <img src="/images/logo.jpg" class="card-img-top" alt="...">
+            <img src="../images/logo.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
                 <p class="card-text">Empresa dedicada a la fabricacion de muebles personalizados con diseños a pedido.</p>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="/images/jonalogo.jpg" class="card-img-top" alt="...">
+            <img src="https://scontent.fzal1-1.fna.fbcdn.net/v/t39.30808-6/285910637_155389080386234_8484541407911336331_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=JflBTmKv_9UAX-MbccO&_nc_ht=scontent.fzal1-1.fna&oh=00_AfDtFUI8WwPyNQ_WpvGH2PuuVW__6IknweI9X-CZdDnGaQ&oe=639C3064" class="card-img-top" alt="Jonathan Morales"/>
             <div class="card-body">
                 <p class="card-text">
                     Emprendedor en creacion de muebles
