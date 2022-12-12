e3ba1f6a5f6ed764b0c1adedbac2d78de7aabf79
FelipeGuzmanL <felipeeduardo.guzmanlobos@gmail.com>
2022-12-12 18:36:36 -0300

    CAMBIOS PARA RESPONSIVE DESIGN

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="container">
+    <!--<div class="container">
         <div class="card" style="width: 18rem;">
             <img src="../images/logo.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
@@ -16,7 +16,19 @@
                 </p>
             </div>
         </div>
-    </div>
+    </div>-->
+
+    <section class="box">
+        <img src="https://scontent.fzal1-1.fna.fbcdn.net/v/t39.30808-6/285910637_155389080386234_8484541407911336331_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=JflBTmKv_9UAX-MbccO&_nc_ht=scontent.fzal1-1.fna&oh=00_AfDtFUI8WwPyNQ_WpvGH2PuuVW__6IknweI9X-CZdDnGaQ&oe=639C3064" width="180"
+         alt="" class="box-img">
+         <h1>Rustic MG</h1>
+         <h2>Empresa de muebles</h2>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto natus quos quibusdam enim laborum asperiores perspiciatis possimus voluptas? Veniam fuga nobis ex incidunt deserunt ut debitis voluptates esse ducimus maiores?</p>
+         <ul>
+            <li><a href="https://www.facebook.com/people/rustic_mg/100081050527630/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="https://www.instagram.com/rustic_mg/?utm_medium=copy_link&hl=es-la" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a></li>
+         </ul>
+    </section>
 
 
 @endsection
